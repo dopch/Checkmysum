d987b0899841931e13f3e86836527249ba811aeb
Thomas Nogueira <thomas.nogueira@digintsolutions.com>
2018-08-14 16:48:33 +0400

    Add of Algo_hashcalc functions to avoid HIGH memory usage due to a single read on large file (Now divides with iter function with sentinel b'' to stop when receive empty binary str), Add multiline to follow Pep8

--- checkmysum.py
+++ checkmysum.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3.6
 ## python3.6 checkmysum.py officialsum file algo
 ##
+
 import argparse
 import hashlib
+
 
 def strcmp(str1: str, str2: str):
     if str1 == str2:
@@ -10,18 +12,52 @@ def strcmp(str1: str, str2: str):
     else:
         print("Checksum DOEST MATCH DROP THIS FILE !")
 
+
+def md5_hashcalc(file) -> str:
+    h_md5 = hashlib.md5()
+    block_size = 65536
+    for data_block in iter(lambda: file.read(block_size), b''):
+            h_md5.update(data_block)
+    return h_md5.hexdigest()
+
+
+def sha1_hashcalc(file) -> str:
+    h_sha1 = hashlib.sha1()
+    block_size = 65536
+    for data_block in iter(lambda: file.read(block_size), b''):
+            h_sha1.update(data_block)
+    return h_sha1.hexdigest()
+
+
+def sha256_hashcalc(file) -> str:
+    h_sha256 = hashlib.sha256()
+    block_size = 65536
+    for data_block in iter(lambda: file.read(block_size), b''):
+            h_sha256.update(data_block)
+    return h_sha256.hexdigest()
+
+
+def sha512_hashcalc(file) -> str:
+    h_sha512 = hashlib.sha512()
+    block_size = 65536
+    for data_block in iter(lambda: file.read(block_size), b''):
+            h_sha512.update(data_block)
+    return h_sha512.hexdigest()
+
+
 def check_digest(args: argparse.Namespace):
     with open(args.Your_file, 'rb') as file:
         if args.Hash_algorithm in ('MD5', 'md5'):
-            strcmp(hashlib.md5(file.read()).hexdigest(), args.Original_checksum)
+            strcmp(md5_hashcalc(file), args.Original_checksum)
         elif args.Hash_algorithm in ('SHA1', 'sha1'):
-            strcmp(hashlib.sha1(file.read()).hexdigest(), args.Original_checksum)
+            strcmp(sha1_hashcalc(file), args.Original_checksum)
         elif args.Hash_algorithm in ('SHA256', 'sha256'):
-            strcmp(hashlib.sha256(file.read()).hexdigest(), args.Original_checksum)
+            strcmp(sha256_hashcalc(file), args.Original_checksum)
         elif args.Hash_algorithm in ('SHA512', 'sha512'):
-            strcmp(hashlib.sha512(file.read()).hexdigest(), args.Original_checksum)
+            strcmp(sha512_hashcalc(file), args.Original_checksum)
         else:
             print(f"{args.Hash_algorithm} not a valid hash algorithm")
+
 
 def main():
     parser = argparse.ArgumentParser(description="Checksum validation tool.")
@@ -30,6 +66,7 @@ def main():
     parser.add_argument('Hash_algorithm', type=str, help="Choose your hask algorith sha1, md5, sha256, sha512.")
     args = parser.parse_args()
     check_digest(args)
+
 
 if __name__ == '__main__':
     main()
